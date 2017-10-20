c62c1b3a57f5c697f679416e1097bca8d371fd7e
Michael Craun <michaelcraun@Michaels-MacBook-Air.local>
2017-10-20 19:41:45 -0400

    lesson 4

---
+++ app/src/main/java/com/craunicproductions/swoosh/Constants.kt
@@ -0,0 +1,7 @@
+package com.craunicproductions.swoosh
+
+/**
+ * Created by michaelcraun on 10/20/17.
+ */
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/craunicproductions/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/craunicproductions/swoosh/LeagueActivity.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,8 +17,33 @@ class LeagueActivity : BaseActivity() {
     }
 
     fun leagueNextClicked(view: View) {
-        val skillIntent = Intent(this, SkillActivity::class.java)
-        startActivity(skillIntent)
+        if (selectedLeague != "") {
+            val skillIntent = Intent(this, SkillActivity::class.java)
+            skillIntent.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillIntent)
+        } else {
+            Toast.makeText(this, "Please select a league.", Toast.LENGTH_SHORT).show()
+        }
     }
 
+    fun onMensClicked(view: View) {
+        womensToggle.isChecked = false
+        coEdToggle.isChecked = false
+
+        selectedLeague = "mens"
+    }
+
+    fun onWomensClicked(view: View) {
+        mensToggle.isChecked = false
+        coEdToggle.isChecked = false
+
+        selectedLeague = "womens"
+    }
+
+    fun onCoedClicked(view: View) {
+        mensToggle.isChecked = false
+        womensToggle.isChecked = false
+
+        selectedLeague = "coed"
+    }
 }
